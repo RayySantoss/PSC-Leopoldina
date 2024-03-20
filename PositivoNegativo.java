eff30a1ca1d951e39d4855cf2a4b42a513ff6a4e
RayySantoss <163216225+RayySantoss@users.noreply.github.com>
2024-03-20 00:30:05 +0000

    Verificar se o número é positivo

---
+++ PositivoNegativo.java
@@ -0,0 +1,17 @@
+public class PositivoNegativo {
+   public static void main (String[] args){
+      int numero = 0;
+
+    
+
+     if(numero > 0){
+       System.out.println( "O número é positivo.");
+     }else if (numero < 0 ){
+      System.out.println( "O número é negativo.");
+     }else{ 
+      System.out.println( "O número é neutro.");
+     } 
+
+    }
+
+}
